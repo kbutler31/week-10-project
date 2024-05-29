309d78f433746c187342125819c50fe458ecd853
Kaleb A Butler <kaleb.butler2010@gmail.com>
2024-05-29 05:21:39 -0500

    finished

--- project/project.js
+++ project/project.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $("#addRowForm").submit(function(event) {
     event.preventDefault();
     var name = $("#nameInput").val();
-    console.log("hello", name)
     var age = $("#ageInput").val();
     var newRow = "<tr><td>" + name + "</td><td>" + age + "</td></tr>";
     $("#tableBody").append(newRow);
